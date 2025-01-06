11a73d9d56f7b702d68ea844bf17af39e9578d04
Herley David Alpizar <herley.david.alpizar@gmail.com>
2025-01-06 15:07:15 -0600

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.education-container').forEach(container => {
-    container.addEventListener('click', () => {
-        const url = container.getAttribute('data-url');
-        window.open(url, '_blank');
-    });
-});
